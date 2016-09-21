649082b696c593d9edcee2c7098625955f17be93
BrightTK <iib11@icloud.com>
2016-09-21 11:17:29 +0700

    การใช้ EditText & String

--- app/src/main/java/sut/momon/allfriend/SignupActivity.java
+++ app/src/main/java/sut/momon/allfriend/SignupActivity.java
@@ -2,12 +2,27 @@ package sut.momon.allfriend;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class SignupActivity extends AppCompatActivity {
+
+    //การประกาศตัวแปร
+
+    private EditText nameEditText,addressEditText,phoneEditText,userEditText,passwordEditText;
+    private String nameString,addressString,phoneString,userString,passwordString,genderString,imageString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        //Bind Winget
+        nameEditText = (EditText) findViewById(R.id.editText);
+        addressEditText = (EditText) findViewById(R.id.editText2);
+        phoneEditText = (EditText) findViewById(R.id.editText3);
+        userEditText = (EditText) findViewById(R.id.editText4);
+        passwordEditText = (EditText) findViewById(R.id.editText5);
+
+
     }
 }
